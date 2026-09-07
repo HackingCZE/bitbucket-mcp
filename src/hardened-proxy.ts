863fa8983b914e99c126e37a40de11bfc9b2e53e
Dominik Hurta <47665187+HackingCZE@users.noreply.github.com>
2026-09-07 13:58:18 +0200

    Load .env in hardened proxy

--- src/hardened-proxy.ts
+++ src/hardened-proxy.ts
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+import "dotenv/config";
 import { spawn } from "node:child_process";
 import { createInterface } from "node:readline";
 import { fileURLToPath } from "node:url";
